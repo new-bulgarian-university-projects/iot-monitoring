729ebafeb30a2c65648749781c78f163f76b001e
nkolchakov <nikola.kolchakov@gmail.com>
2019-06-05 19:37:37 +0300

    add DbContext and WepApi project

---
+++ IoTMon.Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using IoTMon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoTMon.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        { }
+
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // enum type to string
+            modelBuilder.Entity<Sensor>()
+                .Property(s => s.ValueType)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (ValueType)Enum.Parse(typeof(ValueType), v));
+
+            modelBuilder.Entity<Alert>()
+                .Property(a => a.ValueType)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (ValueType)Enum.Parse(typeof(ValueType), v));
+
+            // uniques
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Sensor>()
+                .HasIndex(s => s.Label)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
