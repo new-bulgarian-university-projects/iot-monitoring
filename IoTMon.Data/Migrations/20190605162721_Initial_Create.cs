a5104f72da9a3479e0a32dde7484a3d473e90305
nkolchakov <nikola.kolchakov@gmail.com>
2019-06-05 19:38:30 +0300

    add first migrations

---
+++ IoTMon.Data/Migrations/20190605162721_Initial_Create.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IoTMon.Data.Migrations
+{
+    public partial class Initial_Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DeviceId = table.Column<string>(nullable: true),
+                    SensorId = table.Column<string>(nullable: true),
+                    AlertStarted = table.Column<DateTime>(nullable: false),
+                    AlertClosed = table.Column<DateTime>(nullable: true),
+                    ValueType = table.Column<string>(nullable: true),
+                    TriggeringValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    PasswordSalt = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DeviceId = table.Column<string>(nullable: true),
+                    IsActivated = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    IntervalInSeconds = table.Column<int>(nullable: false),
+                    IsPublic = table.Column<bool>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Label = table.Column<string>(nullable: true),
+                    FriendlyLabel = table.Column<string>(nullable: true),
+                    MeasurementUnit = table.Column<string>(nullable: true),
+                    ValueType = table.Column<string>(nullable: true),
+                    DeviceId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sensors_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_UserId",
+                table: "Devices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_DeviceId",
+                table: "Sensors",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_Label",
+                table: "Sensors",
+                column: "Label",
+                unique: true,
+                filter: "[Label] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alerts");
+
+            migrationBuilder.DropTable(
+                name: "Sensors");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ IoTMon.Data/Migrations/20190605163501_Fix_Add_Nav_Props_To_Alert.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IoTMon.Data.Migrations
+{
+    public partial class Fix_Add_Nav_Props_To_Alert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SensorId",
+                table: "Alerts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeviceId",
+                table: "Alerts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_DeviceId",
+                table: "Alerts",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_SensorId",
+                table: "Alerts",
+                column: "SensorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alerts_Devices_DeviceId",
+                table: "Alerts",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alerts_Sensors_SensorId",
+                table: "Alerts",
+                column: "SensorId",
+                principalTable: "Sensors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alerts_Devices_DeviceId",
+                table: "Alerts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alerts_Sensors_SensorId",
+                table: "Alerts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alerts_DeviceId",
+                table: "Alerts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alerts_SensorId",
+                table: "Alerts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SensorId",
+                table: "Alerts",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeviceId",
+                table: "Alerts",
+                nullable: true,
+                oldClrType: typeof(Guid));
+        }
+    }
+}
