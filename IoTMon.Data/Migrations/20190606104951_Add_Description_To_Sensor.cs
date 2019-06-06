e01f052092f97c77a9ad2d56e854565c8ff9afcc
nkolchakov <nikola.kolchakov@gmail.com>
2019-06-06 17:37:35 +0300

    add description field to Sensors

---
+++ IoTMon.Data/Migrations/20190606104951_Add_Description_To_Sensor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IoTMon.Data.Migrations
+{
+    public partial class Add_Description_To_Sensor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Sensors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Sensors");
+        }
+    }
+}
