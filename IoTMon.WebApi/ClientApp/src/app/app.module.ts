a562b1744f907a632fe747d6c3e40785e43694ac
nkolchakov <nikola.kolchakov@gmail.com>
2019-06-11 19:56:53 +0300

    setup amcharts

--- IoTMon.WebApi/ClientApp/src/app/app.module.ts
+++ IoTMon.WebApi/ClientApp/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
 import { MaterialModule } from './material/material.module';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    ChartComponent,
   ],
   imports: [
     BrowserModule,
